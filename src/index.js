95f4789bb9ee8cc727919c66a947d23e84150126
David Dales <david.c.dales@gmail.com>
2020-09-22 13:26:01 -0400

    OOP, code now in JS file

--- src/index.js
+++ src/index.js
@@ -1,35 +1,136 @@
 import moment from "moment-timezone/moment-timezone";
 import momentTimezoneWithData10YearRange from "./moment-timezone-with-data-10-year-range";
 
-console.log("huh...");
-console.log(momentTimezoneWithData10YearRange);
+document.addEventListener("DOMContentLoaded", function () {
+  var timeZones = {
+    mapProp: null,
+    map: null,
+    x: document.getElementById("demo"),
 
-var d = new Date();
-var n = d.getTime();
+    addMarker(lat, lng) {
+      console.log("adding marker... at ", lat, lng);
 
-console.log(n);
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lng),
+        map: this.map,
+      });
 
-var timezone = "America/Toronto";
-var newLocationTime = moment(n).tz(timezone).format("ha z");
-console.log("newLocationTime:");
-console.log(newLocationTime);
+      // to remove a marker:
+      // marker.setMap(null);
+      // we gotta save these markers to an array on a larger scope
+      // to retain that pointer so we can do that later tho
+    },
 
-// console.dir(moment.tz);
+    addListItem(lat, lng, tz) {
+      console.log("adding list item to HTML");
 
-// var a = moment.tz("2013-11-18 11:55", "America/Los_Angeles");
-// console.log(a);
+      var listItem = document.createElement("li");
+      listItem.innerHTML = `<div style="display: flex;">
+        <p>${lat}, ${lng}</p>
+        <b>${tz}</b>
+      </div>`;
 
-// var june = moment("2014-06-01T12:00:00Z");
-// june.tz("America/Los_Angeles").format("ha z"); // 5am PDT
-// june.tz("America/New_York").format("ha z"); // 8am EDT
-// june.tz("Asia/Tokyo").format("ha z"); // 9pm JST
-// june.tz("Australia/Sydney").format("ha z"); // 10pm EST
+      console.log(document.getElementById("locations"));
 
-// var dec = moment("2014-12-01T12:00:00Z");
-// dec.tz("America/Los_Angeles").format("ha z"); // 4am PST
-// dec.tz("America/New_York").format("ha z"); // 7am EST
-// dec.tz("Asia/Tokyo").format("ha z"); // 9pm JST
-// dec.tz("Australia/Sydney").format("ha z"); // 11pm EST
+      document.getElementById("locations").appendChild(listItem);
+    },
 
-// console.log(june);
-// console.log(dec);
+    addLocation(lat, lng, tz) {
+      this.addMarker(lat, lng);
+      this.addListItem(lat, lng, tz);
+    },
+
+    handleClick(event) {
+      console.log("handleClick...");
+
+      var plugin = this;
+
+      var lat = event.latLng.lat();
+      var lng = event.latLng.lng();
+
+      fetch(
+        `https://api.teleport.org/api/locations/${lat},${lng}/?embed=location:nearest-cities/location:nearest-city/city:timezone/tz:offsets-now%20|%20jq%20%27._embedded.%22location:nearest-cities%22[0]._embedded.%22location:nearest-city%22._embedded.%22city:timezone%22%27`
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          var tz =
+            data._embedded["location:nearest-cities"][0]._embedded[
+              "location:nearest-city"
+            ]._embedded["city:timezone"].iana_name;
+          console.log(tz);
+
+          console.log(plugin);
+          // console.log("addLocation... this: ", this);
+          plugin.addLocation(lat, lng, tz);
+        });
+    },
+
+    showPosition(position) {
+      console.log("showPosition: this is ", this);
+      this.x.innerHTML =
+        "Latitude: " +
+        position.coords.latitude +
+        "<br>Longitude: " +
+        position.coords.longitude;
+
+      console.log(this.map);
+      this.map.setCenter(
+        new google.maps.LatLng(
+          position.coords.latitude,
+          position.coords.longitude
+        )
+      );
+    },
+
+    getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.showPosition.bind(this));
+      } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+    },
+
+    myMap() {
+      this.mapProp = {
+        center: new google.maps.LatLng(51.508742, -0.12085),
+        zoom: 7,
+      };
+      this.map = new google.maps.Map(
+        document.getElementById("googleMap"),
+        this.mapProp
+      );
+      this.getLocation();
+
+      google.maps.event.addListener(
+        this.map,
+        "click",
+        this.handleClick.bind(this)
+      );
+      google.maps.event.addListener(
+        this.map,
+        "touchstart",
+        this.handleClick.bind(this)
+      );
+    },
+
+    init() {
+      this.myMap(); // this calls getLocation... should it?
+
+      this.d = new Date();
+      this.n = this.d.getTime();
+
+      var timezone = "America/Toronto";
+      var newLocationTime = moment(this.n).tz(timezone).format(); //format: "ha z"
+      console.log("newLocationTime:");
+      console.log(newLocationTime);
+    },
+  };
+
+  timeZones.init();
+});
+
+// to-do:
+// use descriptive variable names
+// clean up
